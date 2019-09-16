52af98790be48df94ec195532feff2a486877187
victor7770 <vvalero20363@columbushs.com>
2019-09-16 12:04:00 -0400

    first

--- README.md
+++
@@ -1,3 +0,0 @@
-# webdev-pixel-art
-
-[Pixel Art Webpage](https://codeclubprojects.org/en-GB/webdev/pixel-art/)
---
+++ script.js
@@ -0,0 +1,10 @@
+var penColor = 'black'
+
+function setPenColor(pen){
+    penColor=pen;
+}
+
+function setPixelColor(pixel)
+{
+    pixel.style.backgroundColor = penColor;
+}
